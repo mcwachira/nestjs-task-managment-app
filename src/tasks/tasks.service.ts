856a21cdbcf7f4608d344a2e174a3f77d53fcbd1
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-10 19:36:26 +0300

    Added crud operations to my tasks api

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  //private to prevent changes from being made to the array by any other component that import the service
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+
+  createTask(createTaskDto: CreateTaskDto){
+
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    }
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
