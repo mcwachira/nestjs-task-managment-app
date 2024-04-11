33a830219aa1daac553a47a049a47bb339d32c73
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-11 11:22:38 +0300

    Added error handling and data validation pipes

---
+++ src/tasks/pipes/task-status-validation.pipes.ts
@@ -0,0 +1,32 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+
+export class TaskStatusValidationPipes implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ]
+
+  transform(value: any, metadata: ArgumentMetadata): any {
+    value = value.toUpperCase()
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
+    }
+
+    return value;
+  }
+
+
+  private isStatusValid(status: any) {
+
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
