c58a69ec3de15e9388d5a40056e518c321adf452
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-11 09:05:02 +0300

    Chore:Added filter an search methods to the task api

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -16,6 +17,25 @@ export class TasksService {
     return this.tasks.find((task) => task.id === id);
   }
 
+  getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+
+    const {status, search} = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    return tasks;
+  }
 
   createTask(createTaskDto: CreateTaskDto){
 
