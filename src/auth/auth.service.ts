a6bc27420df7d4e7d94076933dca17067889858b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-15 21:20:23 +0300

    Chore:Added signup feature to the users api

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository : UserRepository,) {}
+
+ signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+
+  }
+}
